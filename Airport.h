92b1461d5681aff402449ecc8418eb193b96baa4
Brianna Zhang <bjzhang2@linux-a2.ews.illinois.edu>
2020-12-01 01:16:33 -0600

    changed Airport into a struct and created & implemented an Edge class where the vertices are represented as Airports

--- Airport.h
+++ Airport.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -44,3 +46,57 @@ private:
     double latitude_;
     double longitude_;
 };
+
+/**
+ * Simple Airport class containing a public double 'latitude' and 'longitude' to represent 2D points 
+ * on a map. Also contains a public string 'name' to represent the name of the Airport. 
+ */
+struct Airport {
+    double latitude_; 
+    double longitude_; 
+    string name_; 
+
+    /**
+     * Default constructor, sets the latitude and longitude to 0.0 and the name to an empty string
+     */
+    Airport() : Airport(0.0, 0.0, "") { }
+
+    /**
+     * Constructor
+     * @param latitude latitude of the Airport
+     * @param longitude longitude of the Airport
+     * @param name name of the Airport
+     */
+    Airport(double latitude, double longitude, string name) : latitude_(latitude), longitude_(longitude), 
+        name_(name) { }
+
+    /**
+     * overload operator < 
+     * @param other the other Airport
+     * @return true for smaller, false otherwise
+     */
+    bool operator<(const Airport& other) const {
+        return (latitude_ < other.latitude_ || longitude_ < other.longitude_); 
+    }
+
+    /**
+     * overload operator ==
+     * @param other the other Airport
+     * @return true for equal, false otherwise 
+     */
+    bool operator==(const Airport& other) const {
+        return (latitude_ == other.latitude_ && longitude_ == other.longitude_ && name_ == other.name_); 
+    }
+}; 
+
+/**
+ * overload operator <<
+ * prints out an Airport
+ * @param out output stream
+ * @param airport the Airport to print
+ * @return output stream
+ */
+inline std::ostream& operator<<(std::ostream& out, Airport const& airport) {
+    out << airport.name_ << ": " << airport.latitude_ << ", " << airport.longitude_; 
+    return out; 
+}
